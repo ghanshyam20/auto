884598e684860c46260a267a02c2d282a850f45d
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-03-13 06:39:45 +0200

    asjj

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
 console.log("Automation project site loaded");
 
 document.querySelectorAll(".gallery img").forEach(img => {
-  img.addEventListener("click", function() {
 
-    const overlay = document.createElement("div");
-    overlay.style.position="fixed";
-    overlay.style.top="0";
-    overlay.style.left="0";
-    overlay.style.width="100%";
-    overlay.style.height="100%";
-    overlay.style.background="rgba(0,0,0,0.9)";
-    overlay.style.display="flex";
-    overlay.style.alignItems="center";
-    overlay.style.justifyContent="center";
-    overlay.style.zIndex="9999";
+img.addEventListener("click", function(){
 
-    const image = document.createElement("img");
-    image.src=this.src;
-    image.style.maxWidth="90%";
-    image.style.maxHeight="90%";
+const overlay=document.createElement("div");
+overlay.style.position="fixed";
+overlay.style.top="0";
+overlay.style.left="0";
+overlay.style.width="100%";
+overlay.style.height="100%";
+overlay.style.background="rgba(0,0,0,0.9)";
+overlay.style.display="flex";
+overlay.style.alignItems="center";
+overlay.style.justifyContent="center";
+overlay.style.zIndex="9999";
 
-    overlay.appendChild(image);
+const image=document.createElement("img");
+image.src=this.src;
+image.style.maxWidth="90%";
+image.style.maxHeight="90%";
 
-    overlay.onclick=()=>{
-        overlay.remove();
-    }
+overlay.appendChild(image);
 
-    document.body.appendChild(overlay);
+overlay.onclick=function(){
+overlay.remove();
+};
 
-  });
+document.body.appendChild(overlay);
+
+});
+
 });
