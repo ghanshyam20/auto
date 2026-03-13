a3b9bc2caecda71306942522ab248f5213175d07
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-03-13 04:27:21 +0200

    now i dont have words in commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 console.log("Automation project site loaded");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", function() {
+
+    const overlay = document.createElement("div");
+    overlay.style.position="fixed";
+    overlay.style.top="0";
+    overlay.style.left="0";
+    overlay.style.width="100%";
+    overlay.style.height="100%";
+    overlay.style.background="rgba(0,0,0,0.9)";
+    overlay.style.display="flex";
+    overlay.style.alignItems="center";
+    overlay.style.justifyContent="center";
+    overlay.style.zIndex="9999";
+
+    const image = document.createElement("img");
+    image.src=this.src;
+    image.style.maxWidth="90%";
+    image.style.maxHeight="90%";
+
+    overlay.appendChild(image);
+
+    overlay.onclick=()=>{
+        overlay.remove();
+    }
+
+    document.body.appendChild(overlay);
+
+  });
+});
